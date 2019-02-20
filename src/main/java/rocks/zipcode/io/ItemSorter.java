28f8de7360e50223f468a442b5966ea4f78996b1
Sean Rowan <seanrowan@zipcoders-MBP.home>
2019-02-20 14:53:24 -0500

    Completed item sorter lab

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,22 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    private Item[] list;
+
+
     public ItemSorter(Item[] items) {
+    this.list = items;
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(list, comparator);
+        return list;
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,29 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+
+    public int compare(Item o1, Item o2) {
+
+        if(o1.getName().compareTo(o2.getName()) > 0){
+            return 1;
+        }else if(o1.getName().compareTo(o2.getName()) < 0){
+            return -1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
